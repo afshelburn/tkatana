4ea451a84011e71d23d47ad0f800c25541e70cc0
Anthony Shelburn <anthony@mercury.lan>
2021-04-01 20:38:49 -0700

    wip-tsl tools

--- librarian.py
+++ librarian.py
@@ -1,8 +1,10 @@
 import json
 import tsl
+import os
+import katana
 
 class Librarian:
-    def __init__(self):#, tkRoot, app, katana):
+    def __init__(self, **kwargs):#, tkRoot, app, katana):
         #self.tkRoot = tkRoot
         #self.app = app
         #self.katana = katana
@@ -17,6 +19,13 @@ class Librarian:
         self.mk2_params[self.sections[4]] = tsl.delay2_tsl
         self.mk2_params[self.sections[5]] = tsl.fx1_tsl
         self.mk2_params[self.sections[6]] = tsl.fx2_tsl
+        
+        #tsl_dir = kwargs['dir']
+        
+    def loadDir(self, tsl_dir):
+        for filename in os.listdir(tsl_dir):
+            if filename.endswith(".tsl"):
+                self.loadTSL(os.path.join(tsl_dir, filename))
         
     def loadTSL(self, file):
         with open(file, 'r') as handle:
@@ -41,8 +50,11 @@ class Librarian:
                     params = self.mk2_params[section]
                     print("patch len = " + str(len(patch)))
                     print("section len = " + str(len(params)))
-                    for i in range(len(patch)):
-                        self.patches[name].append((params[i], patch[i]))    
+                    print(str(patch))
+                    print(str(params))
+                    sz = min(len(patch), len(params))
+                    for i in range(sz):
+                        self.patches[name].append((params[i], int(patch[i],16)))    
                     
         elif 'GT' in self.data['device']:
             print(self.data['device'])
@@ -57,30 +69,31 @@ class Librarian:
                     self.patches[patchName].append((tsl.tsl_params[key], self.data['patchList'][0]['params']))
                 #print(key + " = " + str(self.data['patchList'][0]['params'][key]))
     
-    def loadPatch(katana, patchName, target):
+    def loadPatch(self, katana, patchName, ch=-1):
         params = self.patches[patchName]
-        self.load(katana, patchName, target, params)
+        self.load(katana, patchName, ch, params)
     
-    def load(katana, patchName, ch, params):
-        chAddr = {0:(0x10,0x00),1:(0x10,0x01),2:(0x10,0x02),3:(0x10,0x03),4:(0x10,0x04),5:(0x10,0x05),6:(0x10,0x06),7:(0x10,0x07),8:(0x10,0x08)}
-
+    def load(self, katana, patchName, ch, params):
+        chAddr = {-1:(0x60,0x00),0:(0x10,0x00),1:(0x10,0x01),2:(0x10,0x02),3:(0x10,0x03),4:(0x10,0x04),5:(0x10,0x05),6:(0x10,0x06),7:(0x10,0x07),8:(0x10,0x08)}
+        print("Channel addr = " + str(chAddr[ch]))
         for param in params:
-            addr = (chAddr[ch][0], chAddr[ch][1], param[0][1], param[0][2])
+            print(str(param))
+            addr = (chAddr[ch][0], chAddr[ch][1], param[0][1][0], param[0][1][1])
             offs = param[0][0]
             value = param[1] + offs
             if katana is not None:
                 katana.send_sysex_data(addr, value)
             else:
                 print(str(addr) + " = " + str(value))
-        if katana is not None:
+        if katana is not None and ch > -1:
             katana.set_patch_name(ch, patchName)
         else:
-            print("wrote " + patchName + " to " + str(ch))
-
-            
+            print("done loading patch " + patchName + " into " + str(ch))
         
 l = Librarian()
-l.loadTSL('/home/anthony/git_repos/tkatana/default_mk2.tsl')
-l.loadTSL('/home/anthony/git_repos/tkatana/default.tsl')
-l.loadTSL('/home/anthony/git_repos/tkatana/Time solo.tsl')
+#l.loadTSL('/home/anthony/git_repos/tkatana/default_mk2.tsl')
+#l.loadTSL('/home/anthony/git_repos/tkatana/default.tsl')
+l.loadDir('/home/anthony/tsl_files')
 print(str(l.patches.keys()))
+
+katana = katana.Katana('KATANA MIDI 1',  0,  False)
